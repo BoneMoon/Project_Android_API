738bd12e3218cc9369518f06778b4132e6228cf3
BoneMoon <5josediogo1@gmail.com>
2020-04-13 15:22:30 +0100

    Add migrations

--- database/migrations/2020_04_13_142027_create_notas_table.php
+++ database/migrations/2020_04_13_142027_create_notas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateNotasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,13 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('notas', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('descricao');
+            $table->string('tipodescricao');
+            $table->string('foto');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +30,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('notas');
     }
 }
