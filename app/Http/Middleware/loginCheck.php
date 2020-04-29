bc5e50c08e82cf108ca2ad0f6963b822d727dc59
BoneMoon <5josediogo1@gmail.com>
2020-04-29 19:35:07 +0100

    try2

--- app/Http/Middleware/loginCheck.php
+++ app/Http/Middleware/loginCheck.php
@@ -16,13 +16,11 @@ class loginCheck
      */
     public function handle($request, Closure $next)
     {
-        if ($request->header('Authorization') === null) {
-            return response()->json(['error' => '403'], 403);
+        $user = User::where("api_token", $request->header("Authorization"))->first();
+        if (!$user || $request->header("Authorization") === null) {
+            return response()->json(["Erro" => "Não autorizado!"], 403);
         }
-        $user = User::where('api_token', $request->header('Authorization'))->first();
-        if (!$user) {
-            return response()->json(['error' => '403'], 403);
-        }
+
         return $next($request);
     }
 }
